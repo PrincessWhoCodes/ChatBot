73007b665a3d8be80330fa1931b88da7c0566f95
Vaishnavi Pandya <154917219+PrincessWhoCodes@users.noreply.github.com>
2024-11-28 23:43:35 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,61 @@
+import os
+import openai
+#from dotenv import load_dotenv, find_dotenv
+#_ = load_dotenv(find_dotenv()) # read local .env file
+
+openai.api_key  = "Add your API key"
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, # this is the degree of randomness of the model's output
+    )
+#     print(str(response.choices[0].message))
+    return response.choices[0].message["content"]
+
+def collect_messages(_):
+    prompt = inp.value_input
+    inp.value = ''
+    context.append({'role':'user', 'content':f"{prompt}"})
+    response = get_completion_from_messages(context)
+    context.append({'role':'assistant', 'content':f"{response}"})
+    panels.append(
+        pn.Row('User:', pn.pane.Markdown(prompt, width=600)))
+    panels.append(
+        pn.Row('Assistant:', pn.pane.Markdown(response, width=600, styles={'background-color': '#F6F6F6'})))
+
+    return pn.Column(*panels)
+
+import panel as pn
+pn.extension()
+panels=[]
+
+context=[
+{'role':'system','content':"""
+You are a friendly chatbot, an automated chatt service.\
+you first greet user,then collect information \
+or ask something to user like anything that can i help you,\
+you wait till the response and give effective solution or\
+answers to users. after that ask user for any else favour.\
+"""}]
+inp = pn.widgets.TextInput(value="Hi", placeholder='Enter text here…')
+button_conversation = pn.widgets.Button(name="Chat!")
+
+interactive_conversation = pn.bind(collect_messages, button_conversation)
+
+dashboard = pn.Column(
+    inp,
+    pn.Row(button_conversation),
+    pn.panel(interactive_conversation, loading_indicator=True, height=300),
+)
+dashboard.show()
